816f32c7d74500c8af5b8e2eaf6a054dc3b7246c
Terry <168416857+pdahd@users.noreply.github.com>
2024-10-08 21:02:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1484,7 +1484,8 @@ async function renderHomePage (env, hostName, fragConfigs) {
                 --button-color: #3498DB;
                 --input-background-color: #3C3C3C;
                 --header-shadow: 2px 2px 4px rgba(255, 255, 255, 0.25);
-            }
+                hr { border-color: white; }
+	    }
             .material-symbols-outlined {
                 margin-left: 5px;
                 font-variation-settings:
